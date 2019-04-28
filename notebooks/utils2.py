e9f818cb0a32e2a63542f391d39148ba54b70d88
Nick Landolfi <nclandolfi@gmail.com>
2019-04-27 19:23:31 -0700

    sanity

--- notebooks/utils2.py
+++ notebooks/utils2.py
@@ -1,3 +1,5 @@
+import datetime
+import math
 import json
 import os
 import typing    
@@ -33,34 +35,91 @@ class SLBOIter(object):
         self.trpo_n_episodes = np.array([])
         self.trpo_times = np.array([])
 
-        self.real_mean_evals: np.array([])
-        self.virt_mean_evals: np.array([])
-        self.real_stds_evals: np.array([])
-        self.real_virt_evals: np.array([])
+        self.real_mean_evals = np.array([])
+        self.virt_mean_evals = np.array([])
+        self.real_stds_evals = np.array([])
+        self.virt_stds_evals = np.array([])
+        self.real_eval_times = np.array([])
+        self.virt_eval_times = np.array([])
 
     def append_trpo(self, eps: int, mean: float, std: float, time: int):
-        np.append(self.trpo_n_episodes, eps)
-        np.append(self.trpo_means, mean)
-        np.append(self.trpo_stds, std)
-        np.append(self.trpo_times, time)
+        self.trpo_n_episodes = np.append(self.trpo_n_episodes, eps)
+        self.trpo_means = np.append(self.trpo_means, mean)
+        self.trpo_stds = np.append(self.trpo_stds, std)
+        self.trpo_times = np.append(self.trpo_times, time)
 
-    def append_real(self, mean: float, std: float):
-        np.append(self.real_mean_evals, mean)
-        np.append(self.real_stds_evals, std)
+    def append_real(self, mean: float, std: float, time: int):
+        self.real_mean_evals = np.append(self.real_mean_evals, mean)
+        self.real_stds_evals = np.append(self.real_stds_evals, std)
+        self.real_eval_times = np.append(self.real_eval_times, time)
 
-    def append_virt(self, mean: float, std: float):
-        np.append(self.virt_mean_evals, mean)
-        np.append(self.virt_stds_evals, std)
+    def append_virt(self, mean: float, std: float, time: int):
+        self.virt_mean_evals = np.append(self.virt_mean_evals, mean)
+        self.virt_stds_evals = np.append(self.virt_stds_evals, std)
+        self.virt_eval_times = np.append(self.virt_eval_times, time)
 
+    def is_complete(self) -> bool:
+        return len(self.trpo_means) == self.n_iters * self.n_policy_iters and len(self.real_mean_evals) == self.n_evaluate_iters
+         
     def verify(self):
         assert len(self.trpo_means) == len(self.trpo_stds)
         assert len(self.trpo_stds) == len(self.trpo_n_episodes)
-        assert len(self.trpo_means) == self.n_iters * self.n_policy_iters
 
         assert len(self.real_mean_evals) == len(self.real_stds_evals)
         assert len(self.real_stds_evals) == len(self.virt_mean_evals)
         assert len(self.virt_mean_evals) == len(self.virt_stds_evals)
-        assert len(self.real_mean_evals) == self.n_evaluate_iters
+
+    def plot(self, minrew=None, maxrew=None, avgrew=None, title=None, ylim=None):
+        try:
+            self.verify()
+        except AssertionError as e:
+            print(f"Verify Failed {e}")
+            return
+
+        fig, ax = plt.subplots(figsize=(15, 5))
+
+        mt = min(np.min(self.trpo_times), np.min(self.real_eval_times), np.min(self.virt_eval_times))
+
+        ax.plot(self.trpo_times - mt, self.trpo_means, label="trpo means")
+
+
+        ax.errorbar(
+            self.real_eval_times - mt,
+            self.real_mean_evals,
+            self.real_stds_evals,
+            color='red',
+            fmt='o',
+            label="real evals"
+        )
+
+        ax.errorbar(
+            self.virt_eval_times - mt,
+            self.virt_mean_evals,
+            self.virt_stds_evals,
+            color='blue',
+            fmt='o',
+            label='virtual evals'
+        )
+
+        # if minrew is not None:
+        #     ax.axhline(y=min, color="yellow", label="min")
+        # if maxrew is not None:
+        #     ax.axhline(y=max, color="orange", label="max")
+        # if avgrew is not None:
+        #     ax.axhline(y=avg, color="green", label="avg")
+
+        # ax.set_ylim(-300, 0)
+        if title is not None:
+            ax.set_title(title)
+        ax.legend()
+        ax.set_ylabel("reward")
+
+        if ylim is not None:
+            ax.set_ylim(ylim)
+
+
+
+        #return fig
 
 
 class SLBO(object):
@@ -73,7 +132,11 @@ class SLBO(object):
         self.stages.append(s)
 
 class Run(object):
+    start_time: int
+    
     tasks: typing.List[float]
+
+    post_slbo_eval_means: np.array
 
     # task number, TRPO iteration.
     warms: typing.List[SLBOIter]
@@ -85,11 +148,13 @@ class Run(object):
 
     n_expected_tasks: int
 
-    def __init__(self, n_expected_tasks=None):
+    def __init__(self, start_time: int, n_expected_tasks=None):
+        self.start_time = start_time
         self.n_expected_tasks = n_expected_tasks
         self.tasks = []
         self.warms = []
         self.slbos = []
+        self.post_slbo_eval_means = np.array([])
 
     def append_task(self, t: float): 
         self.tasks.append(t)
@@ -100,75 +165,43 @@ class Run(object):
     def append_slbo(self, s: SLBO):
         self.slbos.append(s)
 
+    def append_post_slbo_eval(self, mean: float, std: float):
+        self.post_slbo_eval_means = np.append(self.post_slbo_eval_means, mean)
+
     def plot_tasks(self):
         plt.stem(self.tasks)
         plt.title(f"Tasks ({len(self.tasks)} of them)")
         plt.xlabel("task #")
         plt.ylabel("goal velocity (speed, really)")
 
-    def plot_warmup(self, i: int, min=None, max=None, avg=None):
-        if i == 0:
-            print("No warmup for first task")
+    def details(self):
+        print(f"Run:\n\t{len(self.tasks)} Tasks so far")
 
+    def plot_timing(self):
+        offset = 0
         fig, ax = plt.subplots(figsize=(15, 5))
-
-        warm = self.warms[9]
-
-        ax.plot(warm.trpo_means)
-
-        eval_spots = [i * warm.n_policy_iters*warm.n_evaluate_iters
-                        for i in range(int(warm.n_iters/warm.n_evaluate_iters))]
-        ax.errorbar(
-            eval_spots,
-            warm.real_mean_evals,
-            warm.real_stds_evals,
-            color='r',
-            fmt='o'
-        )
-
-        ax.errorbar(
-            eval_spots,
-            warm.virt_mean_evals,
-            warm.virt_stds_evals,
-            color='g',
-            fmt='o'
-        )
-
-        if min is not None:
-            ax.axhline(y=min, color="yellow", label="min")
-        if max is not None:
-            ax.axhline(y=max, color="orange", label="max")
-        if avg is not None:
-            ax.axhline(y=avg, color="green", label="avg")
-
-        ax.set_ylim(-300, 0)
-        ax.set_title(f"Warmup #{i}, Task is {self.tasks[i]}")
-        ax.legend()
-        ax.y
-
-
-
-
-
-    if n == 0:
-        return
     
-    fig, ax = plt.subplots(figsize=(15, 5))
+        for i in range(len(self.tasks)):
+            inoff = 0
+            # plot warmup
 
-    plot_stage(ax, warms[jobidx][n], 80, 40)
-    
-    length = min(len(real_warms[jobidx][n]), len(virt_warms[jobidx][n]))
+            ntrpos = len(self.warms[i].trpo_times)
 
-    ax.scatter([(i+1)*40 for i in range(0, length)], real_warms[jobidx][n], color="r")
-    ax.scatter([(i+1)*40 for i in range(0, length)], virt_warms[jobidx][n], color="y")
-    
-    taskvel = tasks[jobidx][n]
-    ax.axhline(y=est(mins, taskvel), color="yellow", label="min")
-    ax.axhline(y=est(maxs, taskvel), color="orange", label="max")
-    ax.axhline(y=est(avgs, taskvel), color="green", label="avg")
-    ax.set_ylim((-300, 0))
-    ax.set_title(f"WARMUP: Task {n}, Velocity: {tasks[jobidx][n]} WARMUPS")
-    ax.legend()
+            ax.plot(offset + inoff + np.arange(ntrpos), self.warms[i].trpo_times - r.start_time, color="blue", label="warmup")
+
+            inoff += ntrpos
+
+            for j in range(len(self.slbos[i].stages)):
+                ininoff = 0
+                stage: SLBOIter= self.slbos[i].stages[j]
+                ntrpos = len(stage.trpo_times)
+
+                ax.plot(offset + inoff + ininoff + np.arange(ntrpos), stage.trpo_times - r.start_time, color="green", label="stage")
+                ininoff += ntrpos
+
+                inoff += ininoff
+
+            offset += inoff       
 
 
 # read a structured log file with lines of JSON to a list of dictionaries.
@@ -179,10 +212,13 @@ def read_log(path):
             lines.append(json.loads(line))
     return lines
 
+def timefrom(s):
+    return datetime.datetime.strptime(s, '%Y-%m-%dT%H:%M:%S.%f')
+
 def from_lines(lines) -> Run:
     task_num = 0
 
-    r = Run()
+    r = Run(timefrom(lines[0]['time']).timestamp())
     warm = None
     slbo = None
     stage = None
@@ -190,43 +226,66 @@ def from_lines(lines) -> Run:
     for l in lines:
         fmt = l['fmt']
         args = l['args']
-        time = l['time']
+        time = timefrom(l["time"])
     
         if "STARTING TASK" in fmt:
             task_num += 1
-            warm = SLBOIter()
+            warm = SLBOIter(40, 40, 1)
             slbo = SLBO()
             r.append_warmup(warm)
             r.append_slbo(slbo)
 
         if "Task Sampled" in fmt or "Task Fixed" in fmt:
-            r.append_task(args[0]["goal_velocity"])
+            if type(args[0]) is str:
+                if "BACKWARD" in args[0]:
+                    r.append_task(-math.inf)
+                if "FORWARD" in args[0]:
+                    r.append_task(math.inf)
+            else:
+                r.append_task(args[0]["goal_velocity"])
 
         if "Starting Stage" in fmt:
-            stage = SLBOIter()
+            stage = SLBOIter(20, 40, 10)
             slbo.append_stage(stage)
 
         if args[0] == "pre-warm-up":
             in_warmup = True
-        if args[0] == "post-warmup":
+        if args[0] == "post-warm-up":
             in_warmup = False
 
         if "[TRPO]" in fmt:
             n_episodes = args[1]
             r_mean = args[2]
             r_std = args[3]
-
             if in_warmup:
-                warm.append_trpo(n_episodes, r_mean, r_std)
+                warm.append_trpo(n_episodes, r_mean, r_std, time.timestamp())
             else:
-                warm.append_trpo(n_episodes, r_mean, r_std)
+                stage.append_trpo(n_episodes, r_mean, r_std, time.timestamp())
+
+        if "# Iter" in fmt:
+            # pull out the model information
+            pass
 
         if "iteration" == args[0]:
             if in_warmup:
                 if args[1] == "Real Env":
-                    warm.append_real(args[3], args[4])
+                    warm.append_real(args[3], args[4], time.timestamp())
                 if args[1] == "Virt Env":
-                    warm.append_virt(args[3], args[4])
+                    warm.append_virt(args[3], args[4], time.timestamp())
+            else:
+                if args[1] == "Real Env":
+                    stage.append_real(args[3], args[4], time.timestamp())
+                if args[1] == "Virt Env":
+                    stage.append_virt(args[3], args[4], time.timestamp())
+
+        if "episode" == args[0]:
+            if in_warmup:
+                raise Exception("shouldn't have episode eval in warmup!")
+
+        if "post-slbo" == args[0]:
+            r.append_post_slbo_eval(args[3], args[4])
+
+            
 
     return r
 
@@ -244,7 +303,7 @@ def remote(job: int):
 def local(job) -> str:
     return  f"/tmp/log{job}.json" 
 
-def load(job: int, reload=True, verbose=True) -> Run:
+def load(job: int, reload=True, verbose=True):
     if reload:
         if verbose:
             print("Loading log file")
@@ -260,4 +319,4 @@ def load(job: int, reload=True, verbose=True) -> Run:
     lines = read_log(local(job))
     if verbose:
         print("Read lines")
-    return from_lines(lines)
+    return (from_lines(lines), lines)
