3112260b34c95343878a46908d95137b123a0a9c
Nick Landolfi <nclandolfi@gmail.com>
2019-05-11 19:39:04 -0700

    d

--- slbo/envs/mujoco/humanoid_task_env.py
+++ slbo/envs/mujoco/humanoid_task_env.py
@@ -23,7 +23,6 @@ class HumanoidTaskEnv(humanoid_task_env.HumanoidTaskEnv, BaseModelBasedEnv):
         data = self.model.data
 
         comvel = self.get_body_comvel("torso")
-        print('A', comvel[0])
         if self._task_config.goal_velocity == -math.inf:
             lin_vel_reward = -comvel[0]
         elif self._task_config.goal_velocity == math.inf:
@@ -50,13 +49,12 @@ class HumanoidTaskEnv(humanoid_task_env.HumanoidTaskEnv, BaseModelBasedEnv):
         scaling = (ub - lb) * 0.5
 
         alive_bonus = 0.2
-        print('B', next_states[:, 36])
         if self._task_config.goal_velocity == -math.inf:
-            lin_vel_reward = -next_states[:, 36]
+            lin_vel_reward = -next_states[..., 36]
         elif self._task_config.goal_velocity == math.inf:
-            lin_vel_reward = next_states[:, 36]
+            lin_vel_reward = next_states[..., 36]
         else:
-            lin_vel_reward = -np.abs(next_states[:, 36] - self._task_config.goal_velocity)  
+            lin_vel_reward = -np.abs(next_states[..., 36] - self._task_config.goal_velocity)  
         ctrl_cost = 5.e-4 * np.square(actions / scaling).sum(axis=1)
         impact_cost = 0.
         vel_deviation_cost = 5.e-3 * np.square(next_states[:, 37:39]).sum(axis=1)
