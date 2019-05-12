7708a950ede6646ceba1483aeb8f84d951d7e513
Nick Landolfi <nclandolfi@gmail.com>
2019-05-11 19:46:04 -0700

    fix np.all

--- main2.py
+++ main2.py
@@ -139,14 +139,14 @@ def main():
         logger.info("### STARTING TASK %d ###", TASK_NUM)
         if FLAGS.task.method == 'random':
             task.sample()
-            if np.all(np.abs(task.goal_velocity)) < 10:
+            if np.all(np.abs(task.goal_velocity) < 10):
                 logger.info('Task Sampled: %s', task)
             else:
                 logger.info('Task Sampled: %s', task.__str__())
         elif FLAGS.task.method == 'fixed':
             assert len(FLAGS.task.fixed_velocities) == FLAGS.task.n_iters, f"{len(FLAGS.task.fixed_velocities)} given velocities, but task.n_iters = {FLAGS.task.n_iters}"
             task.goal_velocity = FLAGS.task.fixed_velocities[TASK_NUM]
-            if np.all(np.abs(task.goal_velocity)) < 10:
+            if np.all(np.abs(task.goal_velocity) < 10):
                 logger.info('Task Fixed: %s', task)
             else:
                 logger.info('Task Fixed: %s', task.__str__())
